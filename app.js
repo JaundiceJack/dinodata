724fb7ea09b39de95b9729a390641aaa685016d0
JaundiceJack <mcneilanjames@gmail.com>
2019-01-09 19:03:40 -0700

    got a temperature and humidity chart set up using static data, next up: use data retrieved from the db

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ db.on('error', (err) => { console.log(err); });
 // Load in database models
 let Snake = require('./models/snake');
 
+// Create a database entry for jormun
+//let jormun = new Snake({userName: "james", password: "gandr", snakeName: "jormun"});
+//jormun.save().then(() => console.log("Jormun saved to HHDB"));
+
 // Instantiate Express
 const app = express();
 // Set the view location and view engine (pug)
@@ -25,8 +29,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 // Make some fake data for the chart
-var fakeTempData = [77, 75, 76, 76, 78, 79, 78];
-var fakeTempChartLabels = ['1/3/19', '1/4/19', '1/5/19', '1/6/19', '1/7/19', '1/8/19', '1/9/19'];
+const fakeLowTempData = [77, 75, 76, 76, 78, 79, 78];
+const fakeHighTempData = [88, 89, 90, 90, 87, 88, 88];
+const fakeHumidData = [55, 57, 65, 59, 60, 67, 65];
+const fakeTempChartLabels = ['1/3/19', '1/4/19', '1/5/19', '1/6/19', '1/7/19', '1/8/19', '1/9/19'];
+const fakeHumidChartLabels = ['1/3/19', '1/4/19', '1/5/19', '1/6/19', '1/7/19', '1/8/19', '1/9/19'];
 // I got the fake data to render (client-side) by passing it in just like i did with Jormun's name
 // So now I'd like to set up how much data to display at once as a guideline of how many days to request and all that
 // I'd like to have some editable options
@@ -48,8 +55,7 @@ app.get('/', (req, res) => {
 // Handle a GET request for Jormun
 app.get("/jormun", (req, res) => {
 	// Obtain the snake in the URL
-	let name = req.url.substr(1)
-	name = name.charAt(0).toUpperCase() + name.substr(1);
+	let name = req.url.substr(1);
 	// Find the snake in the DB
 	Snake.findOne({snakeName: name}, (err, snake) => {
 		if (err) return console.error(err);
@@ -57,7 +63,10 @@ app.get("/jormun", (req, res) => {
 		res.render('index', {
 			snakeName: snake.snakeName,
 			tempChartLabels: fakeTempChartLabels,
-			tempData: fakeTempData
+			lowTempData: fakeLowTempData,
+			highTempData: fakeHighTempData,
+			humidChartLabels: fakeHumidChartLabels,
+			humidData: fakeHumidData
 		});
 	});
 })
