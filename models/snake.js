866240dd2459835486f2988567e19c0f1221cb54
JaundiceJack <mcneilanjames@gmail.com>
2019-01-11 15:03:10 -0700

    set up the schema to track multiple snakes

--- models/snake.js
+++ models/snake.js
@@ -1,29 +1,73 @@
 const mongoose = require('mongoose');
 
+// Things to consider:
+// What if a user has multiple snakes?
+// What if they want to track frozen/prekilled/live prey?
+// I've put a variable under 'food' to track frozen/live
+// to track multiple snakes I'll need to change the user to have an array of snakes
+// I've made a snake schema and replaced snakeName in users to an array of snakes
+// Now what if the user wants to track multiple datapoints through the day?
+
+// The Datum schema now contains a cage key,
+// in the cage key, there is a readings key.
+// readings is an array of Reading's.
+// This way multiple readings can be taken over the course of a day.
+
+// In the User schema, there is now an array of Snake schema in the snakes key.
+// This allows the user to have multiple snakes
+
+// No I need to make a foreign key or similar in the Datum schema, pointing to the snake it's associated with
+// All readings must be associated with at least one snake
+
+// I've added the ObjectId key to the Datum Schema refering to the Snakes Schema
+// I think what I have is coming together, Next I should refine it and attempt reading/writing to the database.
+
+
+let Reading = mongoose.Schema({
+	time: {
+		type: Date,
+		required: true,
+		default: Date.now();
+	}
+	warmSide: {
+		type: Number
+	}
+	coolSide: {
+		type: Number
+	}
+	humidity: {
+		type: Number
+	}
+
+})
+
 // Datum Schema
-let datum = mongoose.Schema({
+let Datum = mongoose.Schema({
+	snakeId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Snakes',
+		required: true
+	} 
 	date: {
 		type: Date,
-		required: true
+		required: true,
+		default: Date.now()
 	},
-	enclosure: {
-		lowTemp: {
-			type: Number
-		},
-		highTemp: {
-			type: Number
-		},
-		humidity: {
-			type: Number
-		},
-		cageCleaned: {
-			type: Boolean
-		},
-		waterBowlCleaned: {
-			type: Boolean
-		},
-		waterBowlFilled: {
-			type: Boolean
+	cage: {
+		readings: [Reading],
+		maintenance: {
+			cageCleaned: {
+				type: Boolean,
+				default: false
+			},
+			bowlCleaned: {
+				type: Boolean,
+				default: false
+			},
+			bowlFilled: {
+				type: Boolean,
+				default: false
+			}
 		}
 	},
 	snake: {
@@ -48,13 +92,29 @@ let datum = mongoose.Schema({
 		},
 		feederWeight: {
 			type: Number
+		},
+		feederLife {
+			type: String	//Can be frozen, prekilled, or live
 		}
 	}
 })
 
+let Snake = mongoose.Schema({
+	name: {
+		type: String,
+		required: true
+	}
+	morph: {
+		type: String
+	}
+	weight: {
+		type: Number
+	}
+})
+
 // Full Schema
-let user = mongoose.Schema({
-	userName: {
+let User = mongoose.Schema({
+	name: {
 		type: String,
 		required: true
 	}
@@ -62,24 +122,30 @@ let user = mongoose.Schema({
 		type: String,
 		required: true
 	},
-	snakeName: {
-		type: String,
-		required: true
-	},
-	options: {
+	preferences: {
 		useCelsius: {
 			type: Boolean,
 			required: false
 		}
 	},
+	snakes: [snake],
 	data: [datum]
 })
 
-let jormun = new user();
-jormun.userName = "james";
-jormun.password = "gandr";
-jormun.snakeName = "jormun";
-jormun.options.useCelsius = true;
+let james = new User({
+	name: "james",
+	password: "gandr",
+	preferences: {
+		useCelsius: true
+	}
+});
+
+let jormun = new Snake({
+	name: "jormun",
+	morph: "Blue Eyed Leucistic"
+});
+
+james.snakes.push(jormun);
 
 let firstDay = new datum();
 firstDay.date = "01/10/2019";
