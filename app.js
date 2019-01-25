220f6c2610d8e8c32d7ba18b3e21ec503f216d40
JaundiceJack <mcneilanjames@gmail.com>
2019-01-25 06:02:42 -0700

    Captured post entries, queried user.

--- app.js
+++ app.js
@@ -121,7 +121,7 @@ How does it complicate once I move the routes outside of this file though?
 */
 
 // Start the server
-const port = 8080;
+const port = 8081;
 app.listen(port, () => {
 	console.log('Server started on port '+port);
 });
