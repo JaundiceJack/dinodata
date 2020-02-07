ca4a35f0957796c972fad933dce727843394e745
JaundiceJack <mcneilanjames@gmail.com>
2020-02-07 08:58:00 -0700

    completing and moving on

--- models/reading.js
+++ models/reading.js
@@ -1,5 +1,9 @@
 // Set up and export a database model to store enclosure readings
 
+// Thoughts: I can establish temperature 1 as hottest and 2 as coldest, but some users may want to observe more than 2 points
+// as well as humidities. So I'll add 10 of each. 20 is rediculous.
+
+
 const mongoose = require('mongoose');
 
 const readingSchema = mongoose.Schema({
@@ -11,14 +15,21 @@ const readingSchema = mongoose.Schema({
 		type: Date,
 		required: true
 	},
-	warm: {
+	temp_1: {
 		type: Number
 	},
-	cool: {
+	temp_2: {
 		type: Number
 	},
-	humidity: {
-		type: Number
+	humid_1: {
+		type: Number,
+		min: 0,
+		max: 100
+	},
+	humid_2: {
+		type: Number,
+		min: 0,
+		max: 100
 	}
 });
 
